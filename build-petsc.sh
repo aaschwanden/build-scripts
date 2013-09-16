2f096d94afcb6742458fcbd42e62227214116573
Andy Aschwanden <aaschwanden@alaska.edu>
2013-09-16 14:07:02 -0800

    - updated pets build script

--- build-petsc.sh
+++ build-petsc.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-PETSC_ARCH=macosx
-
 ## Basic Installation
-# ./configure --with-shared-libraries --with-dynamic-loading --with-fortran=0 --with-blas-lapack-dir=/usr/ --with-cc=/opt/local/bin/openmpicc --with-cxx=/opt/local/bin/openmpic++ --with-mpiexec=/opt/local/bin/openmpiexec --with-mpi-dir=/opt/local --with-clanguage=c++ PETSC_ARCH=$PETSC_ARCH 2>&1 | tee configure.log
 
-## With mumps n' stuff
-./configure --download-parmetis --download-metis --download-scalapack --download-mumps --with-shared-libraries --with-dynamic-loading --with-fortran=1 --with-blas-lapack-dir=/usr/ --download-openmpi --with-clanguage=c++ PETSC_ARCH=$PETSC_ARCH 2>&1 | tee configure.log
+PETSC_ARCH=macosx
+./configure --with-debugging=0 --with-shared-libraries --with-dynamic-loading --with-fortran=0 --with-blas-lapack-dir=/usr/ --with-cc=/opt/local/bin/openmpicc --with-cxx=/opt/local/bin/openmpic++ --with-mpiexec=/opt/local/bin/openmpiexec --with-mpi-dir=/opt/local --with-clanguage=c++ PETSC_ARCH=$PETSC_ARCH
 
 PETSC_DIR=/Users/andy/petsc PETSC_ARCH=$PETSC_ARCH ./config/builder.py 2>&1 | tee compile.log
+
+exit
+
+## With mumps n' stuff
+PETSC_ARCH=macosx-mumps
+./configure --download-parmetis --download-metis --download-scalapack --download-mumps --with-shared-libraries --with-dynamic-loading --with-fortran=1 --with-cc=/opt/local/bin/openmpicc --with-cxx=/opt/local/bin/openmpic++ --with-mpiexec=/opt/local/bin/openmpiexec --with-mpi-dir=/opt/local/ --with-clanguage=c++ PETSC_ARCH=$PETSC_ARCH 2>&1 | tee configure-2.log
+
+PETSC_DIR=/Users/andy/petsc PETSC_ARCH=$PETSC_ARCH ./config/builder.py 2>&1 | tee compile-2.log
