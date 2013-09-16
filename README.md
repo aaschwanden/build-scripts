22a80db50aca3aa0b1d8b7ae5c2f0660a05cfa42
Andy Aschwanden <aaschwanden@alaska.edu>
2013-09-15 20:45:15 -0800

    - added petsc

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ build-scripts
 =============
 
 Build scripts to compile stuff like hdf5, netcdf4, etc, with openmpi/gcc instead of macports openmpi/clang which does not support OpenMP
+
+ln -s /opt/local/bin/openmpic++ ~/local/bin/mpic++
+ln -s /opt/local/bin/openmpicxx ~/local/bin/mpicxx
+ln -s /opt/local/bin/openmpicc ~/local/bin/mpicc
+ln -s /opt/local/bin/openmpif77 ~/local/bin/mpif77
+ln -s /opt/local/bin/openmpif90 ~/local/bin/mpif90
--- build-petsc.sh
+++ build-petsc.sh
@@ -2,6 +2,10 @@
 
 PETSC_ARCH=macosx
 
-./configure --with-shared-libraries --with-dynamic-loading --with-fortran=0 --with-blas-lapack-dir=/usr/ --with-cc=/opt/local/bin/openmpicc --with-cxx=/opt/local/bin/openmpic++ --with-mpiexec=/opt/local/bin/openmpiexec --with-mpi-dir=/opt/local --with-clanguage=c++ PETSC_ARCH=$PETSC_ARCH 2>&1 | tee configure.log
+## Basic Installation
+# ./configure --with-shared-libraries --with-dynamic-loading --with-fortran=0 --with-blas-lapack-dir=/usr/ --with-cc=/opt/local/bin/openmpicc --with-cxx=/opt/local/bin/openmpic++ --with-mpiexec=/opt/local/bin/openmpiexec --with-mpi-dir=/opt/local --with-clanguage=c++ PETSC_ARCH=$PETSC_ARCH 2>&1 | tee configure.log
+
+## With mumps n' stuff
+./configure --download-parmetis --download-metis --download-scalapack --download-mumps --with-shared-libraries --with-dynamic-loading --with-fortran=1 --with-blas-lapack-dir=/usr/ --download-openmpi --with-clanguage=c++ PETSC_ARCH=$PETSC_ARCH 2>&1 | tee configure.log
 
 PETSC_DIR=/Users/andy/petsc PETSC_ARCH=$PETSC_ARCH ./config/builder.py 2>&1 | tee compile.log
