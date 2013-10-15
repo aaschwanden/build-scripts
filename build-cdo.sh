b4640898d6f5cfb57ccd0f075ac357767f775fb3
Andy Aschwanden <aaschwanden@alaska.edu>
2013-10-15 17:34:27 +0200

    - added aspell + english dict to macports

--- build-cdo.sh
+++ build-cdo.sh
@@ -5,12 +5,19 @@ mpiprefix=/Users/andy/local-mpi
 PREFIX=/Users/andy/local
 N=4
 
-CC=${mpiprefix}/bin/mpicc CXX=${mpiprefix}/bin/mpicxx CFLAGS='-fopenmp' CPPFLAGS='-I/usr/include/malloc -I/opt/local/include/udunits2' ./configure --prefix=$PREFIX \
-    --with-netcdf=${PREFIX} \
+#CC=${mpiprefix}/bin/mpicc CXX=${mpiprefix}/bin/mpicxx CFLAGS='-fopenmp' CPPFLAGS='-I/usr/include/malloc -I/opt/local/include/udunits2' ./configure --prefix=$PREFIX \
+#    --with-netcdf=${PREFIX} \
+#    --with-hdf5=${PREFIX} \
+#    --with-proj=${prefix}/lib/proj47 \
+#    --with-udunits2=${prefix} \
+#    --with-zlib=${prefix} 2>&1 | tee configure.log
+
+CC=${PREFIX}/bin/h5pcc CXX=${PREFIX}/bin/h5c++ CFLAGS='-fopenmp' CPPFLAGS='-I/usr/include/malloc -I/opt/local/include/udunits2' ./configure --prefix=$PREFIX \
+    --enable-mpi \
+    --with-netcdf=${PRFIX} \
     --with-hdf5=${PREFIX} \
     --with-proj=${prefix}/lib/proj47 \
     --with-udunits2=${prefix} \
     --with-zlib=${prefix} 2>&1 | tee configure.log
-
 make -j $N 2>&1 | tee compile.log
 make install 2>&1 | tee install.log
