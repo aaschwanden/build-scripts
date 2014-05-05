c59ce0602ea76d58b438e1e34296516864d70548
Andy Aschwanden <andy.aschwanden@gmail.com>
2014-05-04 17:53:21 -0800

    added OpenCV to macports

--- install_macports.sh
+++ install_macports.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 
 sudo port -v install \
-     gcc47 gcc48 \
-     mpich-default \
-     hdf5-18 +mpich +threadsafe \
-     py27-numpy wget \
-     git-core +bash_completion +svn \
-     netcdf +mpich \
-     ncview gsl fftw-3 proj47 cmake \
-     GitX \
-     emacs-mac-app \
-     py27-ipython +notebook +pyside +parallel \
-     py27-pyproj \
-     py27-scipy py27-shapely \
-     py27-cython py27-pyflakes \
-     nco \
-     boost +no_single+no_static+mpich+python27 \
-     swig swig-python \
-     py27-netcdf4 \
-     xinit doxygen graphviz \
-     py27-matplotlib \
-     py27-matplotlib-basemap \
-     gdal +expat+geos+netcdf+python27 \
-     py27-statsmodels py27-acor \
-     py27-pandas \
-     py27-pil \
-     mercurial \
-     udunits2 \
-     vtk5 +python27 \
-     ffmpeg +nonfree \
-     scons \
-     f2c aspell  aspell-dict-en \
-     grass -python26 +python27 \
-     py27-pyqt4 qgis +grass \
-     Py27-virtualenv
+    gcc47 gcc48 \
+    mpich-default \
+    hdf5-18 +mpich +threadsafe \
+    py27-numpy wget \
+    git-core +bash_completion +svn \
+    netcdf +mpich \
+    ncview gsl fftw-3 proj47 cmake \
+    GitX \
+    emacs-mac-app \
+    py27-ipython +notebook +pyside +parallel \
+    py27-pyproj \
+    py27-scipy py27-shapely \
+    py27-cython py27-pyflakes \
+    nco \
+    boost +no_single+no_static+mpich+python27 \
+    swig swig-python \
+    py27-netcdf4 \
+    xinit doxygen graphviz \
+    py27-matplotlib \
+    py27-matplotlib-basemap \
+    bgdal +expat+geos+netcdf+python27 \
+    py27-statsmodels py27-acor \
+    py27-pandas \
+    py27-pil \
+    py27-joblib \
+    mercurial \
+    udunits2 \
+    vtk5 +python27 \
+    ffmpeg +nonfree \
+    scons \
+    f2c aspell  aspell-dict-en \
+    grass -python26 +python27 \
+    py27-pyqt4 qgis +grass \
+    opencv +python27 +qt4 \
+    Py27-virtualenv
 
 
 
