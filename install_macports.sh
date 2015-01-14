896eebc09284e9f4506e5574ca41faf58efc2550
Andy Aschwanden <aaschwanden@alaska.edu>
2015-01-13 15:42:18 -0900

    updated dolfin stuff

--- install_macports.sh
+++ install_macports.sh
@@ -1,42 +1,17 @@
 #!/bin/bash
 
-sudo port -v install \
-    gcc47 gcc48 \
-    mpich-default \
-    hdf5 +mpich +threadsafe \
-    py27-numpy wget \
-    git-core +bash_completion +svn \
-    netcdf +mpich \
-    ncview gsl fftw-3 proj47 cmake \
-    emacs-mac-app \
-    py27-ipython +notebook +pyside +parallel \
-    py27-pyproj \
-    py27-scipy py27-shapely \
-    py27-cython py27-pyflakes \
-    nco \
-    boost +no_single+no_static+mpich+python27 \
-    swig swig-python \
-    py27-netcdf4 \
-    xinit doxygen graphviz \
-    py27-matplotlib \
-    gdal +expat+geos+hdf5+netcdf+python27 \
-    py27-statsmodels py27-acor \
-    py27-pandas \
-    py27-pil \
-    py27-joblib \
-    mercurial \
-    udunits2 \
-    vtk5 +python27 \
-    ffmpeg +nonfree \
-    scons \
-    fondu \
-    saga +python27 py27-saga \
-    f2c aspell  aspell-dict-en \
-    grass -python26 +python27 \
-    py27-pyqt4 qgis +grass \
-    opencv +python27 +qt4 \
-    py27-scikit-image \
-    py27-virtualenv
+sudo port -v install \ gcc47 gcc48 \ mpich-default \ hdf5 +mpich
+    +threadsafe \ py27-numpy wget \ git +bash_completion +svn \ netcdf
+    +mpich \ ncview gsl fftw-3 proj47 cmake \ emacs-mac-app \
+    py27-ipython +notebook +pyside +parallel \ py27-pyproj \
+    py27-scipy py27-shapely \ py27-cython py27-pyflakes \ nco \ swig
+    swig-python \ py27-netcdf4 \ xinit doxygen graphviz \
+    py27-matplotlib \ gdal +spatialite+expat+geos+netcdf+python27 \
+    py27-statsmodels py27-acor \ py27-pandas \ py27-pil \ py27-joblib
+    \ mercurial \ udunits2 \ vtk5 +python27 \ ffmpeg +nonfree \ scons
+    \ fondu \ saga +python27 py27-saga \ f2c aspell aspell-dict-en \
+    grass -python26 +python27 \ py27-pyqt4 qgis +grass \ opencv
+    +python27 +qt4 \ py27-scikit-image \ py27-virtualenv
 
 
 
@@ -48,10 +23,10 @@ exit
 
 # for mumps, you need to edit the port file: remove the -fPIC flag as clang now hard errors on this
 
-sudo port -v install cppunit metis +openmp +gfortran superlu_dist +mpich sundials mumps +mpich +gfortran  petsc +parmetis +gfortran +mumps   boost +mpich eigen3 scotch dolfin +parmetis +scotch
+sudo port -v install cppunit metis +openmp +gfortran superlu_dist +mpich sundials mumps +mpich +gfortran  petsc +parmetis +gfortran +mumps   boost +mpich eigen3 scotch dolfin +parmetis +scotch +petsc
 
 # petsc doesn't compile with suitesparse
 # dolfin doesn't compile with
 
-petsc +parmetis +gfortran +mumps +superlu_dist +suitesparse
+petsc +parmetis +gfortran +mumps +suitesparse
 
