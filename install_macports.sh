82e3987f03e93b8f1fcf332862e0e52acdbdf3c1
Andy Aschwanden <aaschwanden@alaska.edu>
2015-01-21 11:22:42 -0900

    added sqlite to gdal

--- install_macports.sh
+++ install_macports.sh
@@ -6,12 +6,12 @@ sudo port -v install \ gcc47 gcc48 \ mpich-default \ hdf5 +mpich
     py27-ipython +notebook +pyside +parallel \ py27-pyproj \
     py27-scipy py27-shapely \ py27-cython py27-pyflakes \ nco \ swig
     swig-python \ py27-netcdf4 \ xinit doxygen graphviz \
-    py27-matplotlib \ gdal +spatialite+expat+geos+netcdf+python27 \
+    py27-matplotlib \ gdal +sqlite+spatialite+expat+geos+netcdf py27-gdal \
     py27-statsmodels py27-acor \ py27-pandas \ py27-pil \ py27-joblib
     \ mercurial \ udunits2 \ vtk5 +python27 \ ffmpeg +nonfree \ scons
     \ fondu \ saga +python27 py27-saga \ f2c aspell aspell-dict-en \
     grass -python26 +python27 \ py27-pyqt4 qgis +grass \ opencv
-    +python27 +qt4 \ py27-scikit-image \ py27-virtualenv
+    +python27 +qt4 \ py27-scikits-module py27-scikit-image \ py27-virtualenv
 
 
 
