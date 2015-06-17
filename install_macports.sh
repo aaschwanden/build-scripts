b9c4c56f14e193b93093c6ea4ef8bcada9c1ba6b
Andy Aschwanden <aaschwanden@alaska.edu>
2015-06-17 15:33:14 -0800

    updated macports

--- install_macports.sh
+++ install_macports.sh
@@ -3,10 +3,10 @@
 sudo port -v install \ gcc47 gcc48 \ mpich-default \ hdf5 +mpich
     +threadsafe \ py27-numpy wget \ git +bash_completion +svn \ netcdf
     +mpich \ ncview gsl fftw-3 proj47 cmake \ emacs-mac-app \
-    py27-ipython +notebook +pyside +parallel \ py27-pyproj \
+    py27-ipython +notebook +pyside +parallel \ py27-pyproj py27-h5py \
     py27-scipy py27-shapely \ py27-cython py27-pyflakes \ nco \ swig
     swig-python \ py27-netcdf4 \ xinit doxygen graphviz \
-    py27-matplotlib \ gdal +sqlite+spatialite+expat+geos+netcdf py27-gdal \
+    py27-matplotlib \ gdal +expat+geos+grib+hdf5+mpich+netcdf+spatialite+sqlite3+xerces \
     py27-statsmodels py27-acor \ py27-pandas \ py27-pil \ py27-joblib
     \ mercurial \ udunits2 \ vtk5 +python27 \ ffmpeg +nonfree \ scons
     \ fondu \ saga +python27 py27-saga \ f2c aspell aspell-dict-en \
@@ -23,10 +23,9 @@ exit
 
 # for mumps, you need to edit the port file: remove the -fPIC flag as clang now hard errors on this
 
-sudo port -v install cppunit metis +openmp +gfortran superlu_dist +mpich sundials mumps +mpich +gfortran  petsc +parmetis +gfortran +mumps   boost +mpich eigen3 scotch dolfin +parmetis +scotch +petsc
+sudo port -vs install cppunit metis +openmp +gfortran superlu_dist +mpich sundials mumps +mpich +gfortran  petsc +parmetis +gfortran +mumps +suitesparse  boost +mpich eigen3 scotch dolfin +parmetis +scotch +petsc +mpich +suitesparse +hdf5
 
 # petsc doesn't compile with suitesparse
 # dolfin doesn't compile with
 
 petsc +parmetis +gfortran +mumps +suitesparse
-
