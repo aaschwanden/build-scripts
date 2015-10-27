a70c597ee3b2e423554f88f73c8db55f07092d8a
Andy Aschwanden <aaschwanden@alaska.edu>
2015-10-27 13:08:40 -0800

    updated ports

--- install_macports.sh
+++ install_macports.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-sudo port -v install \ gcc47 gcc48 \ mpich-default \ hdf5 +mpich
-    +threadsafe \ py27-numpy wget \ git +bash_completion +svn \ netcdf
-    +mpich \ ncview gsl fftw-3 proj47 cmake \ emacs-mac-app \
-    py27-ipython +notebook +pyside +parallel \ py27-pyproj py27-h5py \
-    py27-scipy py27-shapely \ py27-cython py27-pyflakes \ nco \ swig
-    swig-python \ py27-netcdf4 \ xinit doxygen graphviz \
-    py27-matplotlib \ gdal +expat+geos+grib+hdf5+mpich+netcdf+spatialite+sqlite3+xerces \
-    py27-statsmodels py27-acor \ py27-pandas \ py27-pil \ py27-joblib
-    \ mercurial \ udunits2 \ vtk5 +python27 \ ffmpeg +nonfree \ scons
-    \ fondu \ saga +python27 py27-saga \ f2c aspell aspell-dict-en \
-    grass -python26 +python27 \ py27-pyqt4 qgis +grass \ opencv
-    +python27 +qt4 \ py27-scikits-module py27-scikit-image \ py27-virtualenv
+sudo port -v install \
+    	hdf5 +mpich +threadsafe \
+	
+
+sudo port -v install \ gcc47 gcc48 \ mpich-default \
+     	hdf5 +mpich +threadsafe \
+     	py27-numpy wget \
+     	git +bash_completion +svn \
+     	netcdf +mpich \
+     	ncview gsl fftw-3 proj cmake \ emacs-app \
+     	py27-jupyter +qtconsole \ 
+	py27-pyproj py27-h5py \
+     py27-scipy py27-shapely \ py27-cython py27-pyflakes \
+     nco \
+     swig swig-python \ py27-netcdf4 \ xinit doxygen graphviz \
+     py27-matplotlib \ gdal +expat+geos+grib+hdf5+mpich+netcdf+spatialite+sqlite3 \
+     py27-statsmodels py27-acor \ py27-tables +mpich py27-pandas \ py27-joblib \
+     mercurial \ udunits2  \ ffmpeg +nonfree \ scons \
+     fondu py27-fiona \
+     saga +python27 py27-saga \ f2c aspell aspell-dict-en \
+     grass -python26 +python27 \ py27-pyqt4 qca-oss qgis \
+     py27-scikits-module py27-scikit-image \ py27-virtualenv
 
 
 
